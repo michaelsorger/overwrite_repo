a84eba6f75ddbc86139b80241fd86e251e229fdd
Josh Lipio <u1008246@utah.edu>
2018-09-12 15:13:53 -0600

    Basic moving death wall

---
+++ Overwrite/Assets/Scripts/PathFollower.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour {
+
+    public Vector3 moveTo;
+    public float speed;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveTo, speed);
+        if (transform.position == moveTo)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Destroy(collider.gameObject);
+        }
+    }
+}
---
+++ Overwrite/Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject prefab;
+    public Vector3 startPos;
+    public float spawnTime;
+
+	void Start ()
+    {
+        InvokeRepeating("SpawnObject", 0, spawnTime);
+	}
+	
+    private void SpawnObject()
+    {
+        GameObject obj = Instantiate(prefab, startPos, transform.rotation);
+    }
+}
