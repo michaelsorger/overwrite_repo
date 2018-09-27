0acd1499b1fb3421e7b3da60e88482a8d815eff3
PrivateSkittles <u1008246@utah.edu>
2018-09-26 21:07:08 -0600

    more ui and more sound

--- Overwrite/Assets/Scripts/ScreenFlasher.cs
+++ Overwrite/Assets/Scripts/ScreenFlasher.cs
@@ -8,12 +8,15 @@ public class ScreenFlasher : MonoBehaviour
     // the image you want to fade, assign in inspector
     public Image img;
 
+    public AudioSource whiteNoise;
+
     public float fadeOutTime;
 
     public void OnButtonClick()
     {
         // fades the image out when you click
         StartCoroutine(FadeImage(true));
+        whiteNoise.Play();
     }
 
     IEnumerator FadeImage(bool fadeAway)
@@ -21,12 +24,13 @@ public class ScreenFlasher : MonoBehaviour
         // fade from opaque to transparent
         if (fadeAway)
         {
-            for (float i = 0; i <= 1; i += 2*Time.deltaTime)
-            {
-                // set color with i as alpha
-                img.color = new Color(1, 1, 1, i);
-                yield return null;
-            }
+            //for (float i = 0; i <= 1; i += 2*Time.deltaTime)
+            //{
+            //    // set color with i as alpha
+
+            //    img.color = new Color(1, 1, 1, i);
+            //    yield return null;
+            //}
             // loop over 1 second backwards
             for (float i = 1; i >= 0; i -= Time.deltaTime)
             {
